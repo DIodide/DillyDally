02ff878f239534dc8efec66056f63cb34fb86633
DIodide <ibraheem.amin3@gmail.com>
2025-11-08 03:54:16 -0500

    Refactor API import path and add convexApi module

--- dillydally-frontend/src/App.tsx
+++ dillydally-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useQuery } from "convex/react";
-import { api } from "./lib/api";
+import { api } from "./lib/convexApi";
 
 function App() {
   const tasks = useQuery(api.tasks.get);
