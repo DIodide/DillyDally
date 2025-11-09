c27c654ea2aab6c8b76f0ecd65a184cb12000c44
Fahim H <fahimhussain21@gmail.com>
2025-11-09 01:37:08 -0500

    display snapshots

--- dillydally-frontend/src/App.tsx
+++ dillydally-frontend/src/App.tsx
@@ -7,6 +7,7 @@ import StatsCard from "./components/StatsCard";
 import Insights from "./components/Insights";
 import WebcamDisplay from "./components/WebcamDisplay";
 import MessageBox from "./components/MessageBox";
+import SessionSnapshots from "./components/SessionSnapshots";
 import type { AttentionState } from "./utils/faceTracking/classify";
 import logoImage from "./assets/logo.png";
 import { api } from "./lib/convexApi";
@@ -159,6 +160,9 @@ function App() {
 
         {/* Insights Section */}
         <Insights />
+
+        {/* Session Snapshots Section */}
+        <SessionSnapshots sessionId={sessionId} />
 
         {/* Hidden Session Capture Component - controls screenshot capture */}
         <div style={{ display: "none" }}>
